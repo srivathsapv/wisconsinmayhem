0934d8921533480d9c57394bc666c19e046c8c7a
Srivathsa PV <srivathsa@realimage.com>
2014-11-12 12:27:20 +0530

    added line in footer

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <head>
 	<title>Jerusalem College of Engineering</title>
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" /> 
-	
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
 </head>
 <div id = "header">
 	<center>
 		<a href = "/jce/index.php"><img src = "images/heading.png" height = "75px"></a>
 	</center>
 </div>
+<h1>Hello everyone</h1>
